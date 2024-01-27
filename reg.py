9a1025acaea6775d7680a71789d180e995b6400b
Justice K <vickarol0@gmail.com>
2024-01-27 22:11:16 +1100

    Add regex

---
+++ reg.py
@@ -0,0 +1,22 @@
+import re
+
+s = """
+{ 
+  \key g \major
+  \clef bass 
+  g d' b g 
+  a g c' d' 
+  g e d c \break
+  b, g, a, g,
+  c b, a, g, 
+  c fis g e \break
+  c d e b,
+  c 2 d 2 g, 1}
+"""
+
+matches = re.findall(r"\b(?<!\\key )([a-g])(is|es)*(?:'|,)*(?:\s|$)", s)
+notes = [
+    letter.upper() + alter.replace("es", "b").replace("is", "#")
+    for (letter, alter) in matches
+]
+print(notes)
