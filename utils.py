59ed3c99bc621fa195eb0636462e531e04143e58
Justice K <vickarol0@gmail.com>
2024-02-02 23:34:59 +1100

    Continue writing and rewriting functions.

--- utils.py
+++ utils.py
@@ -1,4 +1,14 @@
+import math
+
 """Utility variables"""
+
+# lists of chromatic scales
+chrom_sharp = ["c", "cis", "d", "dis", "e", "f", "fis", "g", "gis", "a", "ais", "b"]
+chrom_flat = ["c", "des", "d", "ees", "e", "f", "ges", "g", "aes", "a", "bes", "b"]
+
+# circle of fifths
+sharp_keys = ["c", "g", "d", "a", "e", "b", "fis", "cis", "gis"]
+flat_keys = ["f", "bes", "ees", "aes", "des", "ges"]
 
 # pitch classes of white keys
 pitch_class = {
@@ -11,6 +21,7 @@ pitch_class = {
     "b": 11
 }
 
+# corresponding pitch transformations for accidentals
 accidental_terminology = {
     "isis": 2,
     "eses": -2,
@@ -20,27 +31,25 @@ accidental_terminology = {
 
 """Utility functions"""
 
-# SHARPS, -is suffix,
-# FLATS, -es suffix
-# pitch is ABSOLUTE. must denote accidentals EVEN WHEN KEY SIG PRESENT.
-
 # construct_pitch_class: list -> list
 def construct_pitch_class(basso):
     out = []
     for note in basso:
-        change = 0
-        # BROKEN you need a better fix for reading isis/eses
-        for a in accidental_terminology.keys():
-            if a in note:
-                change += accidental_terminology[a]
+        change = accidental_terminology.get(note[1:], 0)
         out.append((pitch_class[note[0]] + change) % 12)
     if len(out) == 12 and sum(out) == 66:
         print("That's a tone row, dawg.")  # lol Schoenberg
     return out
 
-test_bass = ["d", "dis", "disis", "des", "deses"]
-result = construct_pitch_class(test_bass)
-print(result)
+# construct_interval_class: list -> list
+# takes output of construct_pitch_class and returns intervals between notes
+def construct_interval_class(pitches):
+    out = []
+    for i in range(len(pitches) - 1):
+        out.append(int(math.remainder(pitches[i+1] - pitches[i], 12)))
+    out.append(400)  # dummy number to make list the same length as pitch classes
+    return out
+
 
 # generate_absolute_scale: str -> list
 # given keynote, generate scale without accidentals
@@ -48,13 +57,29 @@ def generate_absolute_scale(keynote):
     notes = ["c", "d", "e", "f", "g", "a", "b"]
     return notes[notes.index(keynote[0]):] + notes[:notes.index(keynote[0])]
 
+# generate_major_scale: str -> list
+# given keynote, generate major scale
+def generate_major_scale(keynote):
+    major_scale_pattern = [2, 2, 1, 2, 2, 2, 1]
+    if keynote in sharp_keys:
+        chromatic_notes = chrom_sharp
+    else:
+        chromatic_notes = chrom_flat
+    root_note_index = chromatic_notes.index(keynote)
+    major_scale_notes = [keynote]
+
+    for step in major_scale_pattern:
+        root_note_index = (root_note_index + step) % 12
+        major_scale_notes.append(chromatic_notes[root_note_index])
+    return major_scale_notes
+
 # label_degree: str, str -> int
 # labels according to absolute scale
 def label_degree(degree, key):
     scale = generate_absolute_scale(key)
-    return scale.index(degree)
+    return scale.index(degree[0]) + 1
 
 
-
+"""TESTS"""
 
 
